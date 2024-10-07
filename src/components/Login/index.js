c07e2e1e961543dcdedc4c025b62c8f8b69a39f4
Selma Brattli <selma@brattli.nu>
2024-10-07 18:44:11 +0200

    clean slate for comp

--- src/components/Login/index.js
+++
@@ -1,6 +0,0 @@
-import Button from './Button.jsx'
-import Login from './Login.jsx'
-
-export {
-    Button, Login,
-}
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import {Button, Login} from './components/Login/'
-
-export {
-    Button, Login
-}
